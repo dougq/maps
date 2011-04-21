6dd700698d086fa14ab7a4885fdaffd8588e05d0
Doug Quirke <doug@ocwap.org>
2011-04-21 12:00:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maps::Application.config.secret_token = '136c68ee339feabc586b27192dc8b091fa081c2d588ee60644f8818a3de76702f24345e9a9d5ddf141b612fd1a6c947f21137e570d61f2c6cbd83d819a2f7e84'
---
+++ public/javascripts/application.js
@@ -0,0 +1,125 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+var centerLatitude = 37.4419;
+var centerLongitude = -122.1419;
+var startZoom = 12;
+ 
+var map;
+
+function init() 
+{
+	if (GBrowserIsCompatible()) {
+      	map = new GMap2(document.getElementById("map_canvas"));
+listMarkers();
+		map.addControl(new GSmallMapControl());
+map.addControl(new GMapTypeControl());
+map.setCenter(new GLatLng(centerLatitude, centerLongitude), startZoom);
+ 
+GEvent.addListener(map, "click", function(overlay, latlng) {//only perform the click if the window is closed and the click was directly on the map.
+if(!overlay) {
+//create an HTML DOM form element
+var inputForm = document.createElement("form");
+inputForm.id='geocache-input';
+inputForm.setAttribute("action","");
+inputForm.onsubmit = function() {storeMarker(); return false;};
+ 
+//retrieve the longitude and lattitude of the click point
+var lng = latlng.lng();
+var lat = latlng.lat();
+ 
+inputForm.innerHTML = '<fieldset style="width:150px;">'
++ '<legend>New Marker</legend>'
++ '<label for="found">Found</label>'
++ '<input type="text" id="found" name="m[found]" style="width:100%;"/>'
++ '<label for="left">Left</label>'
++ '<input type="text" id="left" name="m[left]" style="width:100%;"/>'
++ '<label for="left">Icon URL</label>'
++ '<input type="text" id="icon" name="m[icon]" style="width:100%;"/>'
++ '<input type="submit" value="Save"/>'
++ '<input type="hidden" id="longitude" name="m[lng]" value="' + lng + '"/>'
++ '<input type="hidden" id="latitude" name="m[lat]" value="' + lat + '"/>'
++ '</fieldset>';
+ 
+map.openInfoWindow (latlng,inputForm);
+}
+
+});
+}    }
+
+function storeMarker(){
+	var lng = $("longitude").value;
+	var lat = $("latitude").value;
+	var formValues=Form.serialize('geocache-input');
+	new Ajax.Request( 'create',
+		{ method: 'post',
+			parameters: formValues,
+			onComplete: function(request){
+              //parse the result to JSON (simply by eval-ing it)
+              res=eval( "(" + request.responseText + ")" );
+
+            //check to see if it was an error or success
+            if(!res.success) {
+                alert(res.content);
+            } else {
+                //create a new marker and add its info window
+                var latlng = new GLatLng(parseFloat(lat),parseFloat(lng));
+
+                var marker = createMarker(latlng, res.content, res.icon);
+                map.addOverlay(marker);
+                map.closeInfoWindow();
+            }
+        }
+    });
+}
+
+function createMarker(latlng, html, iconImage) {
+	if(iconImage!='') {
+
+		var icon = new GIcon();
+		icon.image = iconImage;
+		icon.iconSize = new GSize(25,25);
+		icon.inconAnchor = new GPoint(14,25);
+		icon.infoWindowAnchor = new GPoint(14,14);
+	     var marker = new GMarker(latlng);
+		} else {
+			var marker = new GMarker(latlng);
+		}
+     GEvent.addListener(marker, 'click', function() {
+          var markerHTML = html;
+          marker.openInfoWindowHtml(markerHTML);
+    });
+    return marker;
+}
+
+function listMarkers() {
+  var request = GXmlHttp.create();
+  //tell the request where to retrieve data from.
+  request.open('GET', 'list', true);
+  //tell the request what to do when the state changes.
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      //parse the result to JSON,by eval-ing it.
+      //The response is an array of markers
+      markers=JSON.parse(request.responseText);
+      for (var i = 0 ; i < markers.length ; i++) {
+        var lat=markers[i].lat;
+        var lng=markers[i].lng;
+var iconImage = markers[i].icon;
+        //check for lat and lng so MSIE does not error
+        //on parseFloat of a null value
+        if (lat && lng) {
+        var latlng = new GLatLng(parseFloat(lat),parseFloat(lng));
+        var html = '<div><b>Found</b> ' + markers[i].found + '</div><div><b>Left</b> '
+                  + markers[i].left + '</div>';
+	
+        var marker = createMarker(latlng, html, iconImage);
+        map.addOverlay(marker);
+        } // end of if lat and lng
+      } // end of for loop
+    } //if
+  } //function
+  request.send(null);
+}
+
+window.onload = init;
+window.onunload = GUnload;
